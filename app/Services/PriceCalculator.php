aed9eb3b6670d5b5a0b173c185d87d17eb33b9e2
Alberto <alberrto.ramos@gmail.com>
2026-07-28 21:14:27 -0600

    Ejemplo ci-cd

---
+++ app/Services/PriceCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+class PriceCalculator
+{
+    public function applyDiscount(float $price, float $percent): float
+    {
+        $discount = $price * ($percent / 100);
+
+        return round($price - $discount, 2);
+    }
+}
---
+++ tests/Unit/PriceCalculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\PriceCalculator;
+use PHPUnit\Framework\TestCase;
+
+class PriceCalculatorTest extends TestCase
+{
+    public function test_aplica_descuento_correctamente(): void
+    {
+        $calc = new PriceCalculator();
+
+        $this->assertEquals(90.00, $calc->applyDiscount(100, 10));
+    }
+
+    public function test_sin_descuento_devuelve_el_precio_original(): void
+    {
+        $calc = new PriceCalculator();
+
+        $this->assertEquals(50.00, $calc->applyDiscount(50, 0));
+    }
+}
