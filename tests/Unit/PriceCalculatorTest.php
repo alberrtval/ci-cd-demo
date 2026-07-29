945abb2064f1499d32b525f74dea874dca12db0b
Alberto <alberrto.ramos@gmail.com>
2026-07-28 21:24:59 -0600

    Aplicar estilo d codigo con pint

--- tests/Unit/PriceCalculatorTest.php
+++ tests/Unit/PriceCalculatorTest.php
@@ -9,14 +9,14 @@ class PriceCalculatorTest extends TestCase
 {
     public function test_aplica_descuento_correctamente(): void
     {
-        $calc = new PriceCalculator();
+        $calc = new PriceCalculator;
 
         $this->assertEquals(90.00, $calc->applyDiscount(100, 10));
     }
 
     public function test_sin_descuento_devuelve_el_precio_original(): void
     {
-        $calc = new PriceCalculator();
+        $calc = new PriceCalculator;
 
         $this->assertEquals(50.00, $calc->applyDiscount(50, 0));
     }
